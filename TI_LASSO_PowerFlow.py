b4506eda80557d2c332fb7a4f67e09451083345f
linshen1213 <linshen1213@163.com>
2022-12-27 11:18:47 +0800

    提交文件

---
+++ TI_LASSO_PowerFlow.py
@@ -0,0 +1,135 @@
+# -*- coding: UTF-8 -*-
+"""
+@Date  : 2022/5/24 10:32 
+@Author: Chongyan
+@Aiming: Distribution Grid Topology Identification with LASSO, LS, RIDGE and other methods
+"""
+
+import pandas as pd
+import numpy as np
+import random as rnd
+from scipy import sparse
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.linear_model import Lasso
+import networkx as nx
+from sklearn.linear_model import LassoCV
+
+# 根据基尔霍夫定理构造数据集  I_i = sigma(V_i - V_j) * 1/r_ij
+# N节点， M个时间点数据, NxN 维的r阻抗数据；记录M个时间点的 各节点的电流 电压数据， 反推 r_ij
+# V_i = Vhat* sin(w*t + w_i); Vhat=220, w=50, t = 1,2,...,M, w_i 随机选取[0,2*pi]
+# r_ij 得确保连通性 和 稀疏性
+
+cid = rnd.seed(1)
+
+def generate_data(n, m):
+    # 利用 networkx的树图 保障r_ij的连通性
+    # vhat, w = 220, 50
+    # wi = np.random.uniform(low=0, high=6, size=(1, n))
+    # t = np.array(list(range(1, m+1)))
+    # G = nx.random_powerlaw_tree(n=n, gamma=3, seed=cid, tries=10000)
+    G = nx.connected_watts_strogatz_graph(n=n, k=6, p=0.5, tries=100)
+    print(nx.is_connected(G))
+    # nx.draw(G)
+    # plt.show()
+    adj_mat = nx.adjacency_matrix(G)
+    r = sparse.csr_matrix(np.multiply(adj_mat.todense(), np.random.uniform(low=2, high=4, size=(n, n))))
+    #v = vhat * np.sin(w * t.reshape(m, 1).repeat(n, axis=1) + wi.repeat(m, axis=0))
+    v = np.random.normal(loc=220, scale=10, size=(m, n))
+
+    Iflow = np.zeros((m, n))
+    for i in range(m):
+        for j in range(n):
+            neighb = G.neighbors(j)
+            for item in neighb:
+                Iflow[i, j] += (v[i, j]-v[i, item])/r[j, item]
+    np.savetxt(f"data/WSG_adj_m{m}_n{n}.csv", adj_mat.todense(), delimiter=",", fmt='%d')
+    np.savetxt(f"data/WSG_r_m{m}_n{n}.csv", r.todense(), delimiter=",", fmt='%.2f')
+    np.savetxt(f"data/WSG_v_m{m}_n{n}.csv", v, delimiter=",", fmt='%.2f')
+    np.savetxt(f"data/WSG_iflow_m{m}_n{n}.csv", Iflow, delimiter=",", fmt='%.2f')
+    return adj_mat, r, v, Iflow
+
+
+def generate_data_dcpf(): # 用直流潮流数据替换上面生成的试验数据
+
+    pass
+
+
+def graph_connected():
+    adj = pd.read_csv(f'data/WSG_adj_m{m}_n{n}.csv', header=None)
+    adj = np.matrix(adj)
+    G = nx.from_numpy_matrix(adj)
+    return nx.is_connected(G)
+
+
+def dataset_nodei(nodeIndex, m, n, percent):
+    # 按节点构造 y 和 Theta 矩阵 y|mx1 = Ii|mx1, Theta|mxn, Theta_i[m,j] = v[m,i]-v[m,j], j∈[1,n]. xij=1/rij
+    # 注意这里的下标i是固定的，指代节点i，这一次的拓扑辨识都是针对节点i的，xij!=0，则说明i与节点j有关联。xij是要回归的系数
+    v_mn = pd.read_csv(f'data/WSG_v_m{m}_n{n}.csv', header=None)
+    dfx = v_mn.sub(v_mn.iloc[:, nodeIndex], axis=0) * -1
+    dfx.drop(columns=[nodeIndex], axis=1, inplace=True)
+    i_mn = pd.read_csv(f'data/WSG_iflow_m{m}_n{n}.csv', header=None)
+    dfy = i_mn.iloc[:, nodeIndex]
+    mask = rnd.sample(range(m), int(m*percent))
+    dfx = dfx.iloc[mask, :]
+    dfy = dfy.iloc[mask]
+
+    return dfx, dfy
+
+
+def TI_lasso(pct, nodeIndex, m, n):
+
+    X, y = dataset_nodei(nodeIndex=nodeIndex, m=m, n=n, percent=pct)
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, random_state=10)
+    model = LassoCV(cv=5, random_state=10, max_iter=10000)
+    model.fit(X_train, y_train)
+    lasso_best = Lasso(alpha=model.alpha_)
+    lasso_best.fit(X_train, y_train)
+    return list(zip(lasso_best.coef_, X))
+
+
+# 电压相关性检测
+def volt_correlation(nodeIndex,m,n):
+    v_mn = pd.read_csv(f'data/WSG_v_m{m}_n{n}.csv', header=None)
+    dfx = v_mn.sub(v_mn.iloc[:, nodeIndex], axis=0) * -1
+    dfx.drop(columns=[nodeIndex], axis=1, inplace=True)
+    base = dfx[dfx.columns[0]]
+    x, y = [], []
+    for item in range(1, dfx.shape[1]):
+        x.append(dfx.columns[item])
+        y.append(base.corr(dfx[dfx.columns[item]]))
+        plt.scatter(x, y, alpha=0.5)
+        plt.xlabel("Node Label")
+        plt.ylabel("Pearson Correlation")
+        plt.show()
+
+    return None
+
+
+
+def classify_result(pct, m, n):
+    xaxis, yaxis, color = [], [], []
+    for node in range(n):
+        coef = TI_lasso(pct, node, m, n)
+        adj = pd.read_csv(f'data/WSG_adj_m{m}_n{n}.csv', header=None)
+        for item in coef:
+            xaxis.append(node)
+            yaxis.append(item[0])
+            if adj.iloc[node, item[1]] == 1:
+                color.append('r')
+            else:
+                color.append('g')
+    plt.scatter(xaxis, yaxis, c=color, alpha=0.5)
+    plt.xlabel(f"Node Label with {100*pct}% Measurements ")
+    plt.ylabel("Coefficient")
+
+    plt.show()
+
+
+
+n, m = 100, 200
+pct = 0.2
+#volt_correlation(3,m,n)
+#adj, r, v, iflow = generate_data(n, m)
+# classify_result(pct, m, n)
+gtree = nx.balanced_tree(4, 3)
---
+++ powerflow/data_from_pandapower_version2.py
@@ -0,0 +1,109 @@
+# -*- coding: UTF-8 -*-
+"""
+@Date  : 2022/8/23 14:19 
+@Author: Chongyan
+@Aiming:
+"""
+import pandas as pd
+import numpy as np
+import os
+import sys
+import pandapower as pp
+
+
+global df
+df = pd.read_csv(f'IEEE69.csv')
+
+def random_load(times, busNum):
+    v = np.random.normal(loc=1, scale=1, size=(times, busNum))
+    # v = np.where(v > 0, v, -v) # 把负load改为正的
+    return v
+
+
+def build_case69(rnd_load, timeN):
+    net = pp.create_empty_network()
+    # build 69+1 buses
+    # df = pd.read_csv(f'IEEE69.csv')
+    for i in range(70):
+        if i == 0:
+            pp.create_bus(net, vn_kv=33)
+        else:
+            pp.create_bus(net, vn_kv=11)
+    pp.create_ext_grid(net, bus=0, vm_pu=1.02)
+    # create lines based on the df, and add loads to the buses
+    for index, row in df.iterrows():
+
+        pp.create_line_from_parameters(net, from_bus=int(row['from']), to_bus=int(row['to']), length_km=1,
+                                       r_ohm_per_km=float(row['rohm']), x_ohm_per_km=float(row['xohm']), c_nf_per_km=0,
+                                       max_i_ka=float(row['maxi']) / 100.0)
+        # pp.create_load(net, bus=int(row['to']), p_mw=P_incr+float(row['P'])/1000, q_mvar=Q_incr+float(row['Q'])/1000)
+    for i in range(rnd_load.shape[1]):
+        pp.create_load(net, bus=i+1, p_mw=rnd_load[timeN][i])
+    # add the transformer
+    pp.create_transformer_from_parameters(net, hv_bus=0, lv_bus=1, sn_mva=6.0, vn_hv_kv=33.0, vn_lv_kv=11.0,
+                                          vkr_percent=0.25, vk_percent=12.0, pfe_kw=30, i0_percent=0.06)
+    return net
+
+
+def power_thru_bus(rnd_load, timeN):
+
+    net = build_case69(rnd_load, timeN)
+    pp.rundcpp(net)
+    resBus = net.res_bus
+    resLine = net.res_line
+    bus_to_ID = {}   # 记录 {fromBus:[(toBus,busID),...]}
+    bus_load = {}
+    for i in range(df.shape[0]):
+        from_bus = int(df.iloc[i]['from'])
+        to_bus = int(df.iloc[i]['to'])
+        # load = P_incr + float(df.iloc[i]['P'])/1000
+        bus_to_ID.setdefault(from_bus, [])
+        bus_to_ID[from_bus].append((to_bus, i))
+        # bus_load[to_bus] = load
+    bus_P = {}  # 计算一个断面中各个bus的功率和相角
+    bus_Va = {}
+    for i in range(1, resBus.shape[0]):
+        pi = 0
+        if i in bus_to_ID:
+            pi += sum([float(resLine.iloc[j[1]]['p_from_mw']) for j in bus_to_ID[i]])
+        pi += rnd_load[timeN][i-1]
+        bus_P[i] = round(pi, 6)
+        bus_Va[i] = round(float(resBus.iloc[i]['va_degree']), 6)
+
+    return bus_P, bus_Va
+
+
+
+def generate_69adj():
+
+    adj = [[0 for i in range(69)] for j in range(69)]
+    for i in range(df.shape[0]):  # 注意下标与编号的-1关系
+        from_bus = int(df.iloc[i]['from'])-1
+        to_bus = int(df.iloc[i]['to'])-1
+        adj[from_bus][to_bus], adj[to_bus][from_bus] = 1, 1
+    pd.DataFrame(adj).to_csv('data/69_adj.csv', index=False, header=0)
+
+
+def generate_P_Va():
+
+    times = 200
+    busNum = 69
+    rndload = random_load(times, busNum)
+    P_set = []
+    Va_set = []
+    for i in range(times):
+        print(i,'/',times)
+        # P_incr = i/1000.0
+        P, Va = power_thru_bus(rndload, i)
+        P_set.append(P.values())
+        Va_set.append(Va.values())
+    pd.DataFrame(P_set).to_csv('data/dcpf_69_P.csv', index=False, header=0)
+    pd.DataFrame(Va_set).to_csv('data/dcpf_69_Va.csv', index=False, header=0)
+# generate_69adj()
+generate_P_Va()
+print('done')
+
+
+
+
+
